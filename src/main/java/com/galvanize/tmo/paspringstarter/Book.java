3da7102bfac0022b7fd33db47589ba4902250599
NNguyen61 <NNguyen61@TM0612300.gsm1900.org>
2021-08-23 18:22:52 -0700

    try fix id

--- src/main/java/com/galvanize/tmo/paspringstarter/Book.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/Book.java
@@ -3,12 +3,13 @@ package com.galvanize.tmo.paspringstarter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Book {
 	
-	private @Id @GeneratedValue Long id;
+	private @Id @GeneratedValue(strategy = GenerationType.AUTO) Long id;
 	private String author;
 	private String title;
 	private Long yearPublished;
