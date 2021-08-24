33ebb2cd71a4fe93a0e8be3e0287329ab71c047d
NNguyen61 <NNguyen61@TM0612300.gsm1900.org>
2021-08-23 18:42:47 -0700

    atomic integer try

--- src/main/java/com/galvanize/tmo/paspringstarter/Book.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/Book.java
@@ -4,9 +4,6 @@ package com.galvanize.tmo.paspringstarter;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Book {
